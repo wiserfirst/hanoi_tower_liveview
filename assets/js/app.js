72cf91684daca88645793c01ded6ffa08445867c
Qing Wu <wiserfirst@gmail.com>
2019-10-29 10:25:50 +1100

    first attempt for drag-and-drop

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,5 +14,59 @@ import "phoenix_html"
 import {Socket} from "phoenix"
 import {LiveSocket} from "phoenix_live_view"
 
-let liveSocket = new LiveSocket("/live", Socket)
+var dragged;
+
+const Hooks = {
+  Disk: {
+    mounted() {
+      this.el.addEventListener("dragstart", function( event ) {
+        // store a ref. on the dragged elem
+        dragged = event.target;
+        // make it half transparent
+        event.target.style.opacity = .7
+        const rod_no = event.target.closest(".subcontainer").getAttribute("phx-value-rodno")
+        console.log(rod_no)
+
+        this.pushEvent("mark-rod", rod_no)
+      }, false)
+
+      this.el.addEventListener("dragend", function( event ) {
+        // reset the transparency
+        event.target.style.opacity = ""
+      }, false)
+
+    }
+  },
+  DropContainer: {
+    mounted() {
+      /* events fired on the drop targets */
+      this.el.addEventListener("dragover", function( event ) {
+        // prevent default to allow drop
+        event.preventDefault()
+      }, false)
+
+      this.el.addEventListener("dragenter", function( event ) {
+        // highlight potential drop target when the draggable element enters it
+        event.target.style.background = "lightgrey"
+      }, false)
+
+      this.el.addEventListener("dragleave", function( event ) {
+        // reset background of potential drop target when the draggable element leaves it
+        event.target.style.background = ""
+      }, false)
+
+      this.el.addEventListener("drop", function( event ) {
+        // prevent default action (open as link for some elements)
+        event.preventDefault()
+        // move dragged elem to the selected drop target
+        event.target.style.background = ""
+        const rod_no = event.target.getAttribute("phx-value-rodno")
+        console.log(rod_no)
+        pushEvent("mark-rod", rod_no)
+      }, false)
+    }
+  }
+}
+
+let liveSocket = new LiveSocket("/live", Socket, {hooks: Hooks})
 liveSocket.connect()
